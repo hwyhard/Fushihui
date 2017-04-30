38a69bf57098ac13505ad4c67dbc30d92c8d817b
ripple <hwyhard@163.com>
2017-04-30 23:29:38 +0800

    add splash screen

---
+++ fushihui/app/src/main/java/com/hwyhard/www/fushihui/activity/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.hwyhard.www.fushihui.activity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.hwyhard.www.fushihui.R;
+
+public class SplashActivity extends AppCompatActivity {
+    ImageView imageView;
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        imageView = (ImageView) findViewById(R.id.splash_image);
+        Glide.with(SplashActivity.this)
+                .load(R.drawable.app_cover_last)
+                .into(imageView);
+        handler = new Handler();
+        //设置欢迎页时间
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        },2000);//2000ms
+
+    }
+}
